f20db6b9b45b5137c67d7f30d773996581a61340
Phuoc Sanh <sanh.tp@mekongsoft.com.vn>
2025-03-26 15:16:04 +0700

    đăng kí sw

--- src/main.tsx
+++ src/main.tsx
@@ -34,61 +34,61 @@ import './dx.material.custom-scheme.css';
 //     });
 // }
 
-// if ('serviceWorker' in navigator) {
-//   await navigator.serviceWorker.getRegistrations().then(registrations => {
-//     const isRegistered = registrations.some(reg =>
-//       reg.active?.scriptURL.includes('firebase-messaging-sw.js')
-//     );
-//     if (!isRegistered) {
-//       navigator.serviceWorker
-//         .register('/firebase-messaging-sw.js', { scope: "/" })
-//         .then(async registration => {
-//           console.log('Service Worker registered:', registration);
-//           const readyRegistration = await navigator.serviceWorker.ready;
-//           const worker = registration.installing || registration.waiting || registration.active;
-//           if (worker) {
-//             worker.addEventListener('statechange', e => {
-//               console.log('Service Worker state:', (e.target as ServiceWorker)?.state);
-//             });
-//           }
+if ('serviceWorker' in navigator) {
+  await navigator.serviceWorker.getRegistrations().then(registrations => {
+    const isRegistered = registrations.some(reg =>
+      reg.active?.scriptURL.includes('firebase-messaging-sw.js')
+    );
+    if (!isRegistered) {
+      navigator.serviceWorker
+        .register('/firebase-messaging-sw.js', { scope: '/' })
+        .then(async registration => {
+          console.log('Service Worker registered:', registration);
+          const readyRegistration = await navigator.serviceWorker.ready;
+          const worker = registration.installing || registration.waiting || registration.active;
+          if (worker) {
+            worker.addEventListener('statechange', e => {
+              console.log('Service Worker state:', (e.target as ServiceWorker)?.state);
+            });
+          }
 
-//           if (readyRegistration.active) {
-//             console.log('Service Worker đang active');
-//             // readyRegistration.active.postMessage({
-//             //   type: 'INIT_FIREBASE',
-//             //   // config: {
-//             //   //   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-//             //   //   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-//             //   //   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-//             //   //   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-//             //   //   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-//             //   //   appId: import.meta.env.VITE_FIREBASE_APP_ID,
-//             //   //   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
-//             //   // },
-//             //   config: {
-//             //     apiKey: 'AIzaSyD3Ck4f7Q3PIJ_WRqIdsRagwbISjrrBAGM',
-//             //     authDomain: 'bqlda-hoc-mon.firebaseapp.com',
-//             //     projectId: 'bqlda-hoc-mon',
-//             //     storageBucket: 'bqlda-hoc-mon.firebasestorage.app',
-//             //     messagingSenderId: '568867220435',
-//             //     appId: '1:568867220435:web:973b81f944108377c6dd26',
-//             //     measurementId: 'G-334Q9F6EMG',
-//             //   },
-//             // });
-//           }
-//         })
-//         .catch(error => {
-//           console.error('Đăng ký thất bại:', error);
-//         });
-//     } else {
-//       console.log('Service Worker đã được đăng ký trước đó');
-//     }
-//   });
-// }
+          if (readyRegistration.active) {
+            console.log('Service Worker đang active');
+            // readyRegistration.active.postMessage({
+            //   type: 'INIT_FIREBASE',
+            //   // config: {
+            //   //   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+            //   //   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+            //   //   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+            //   //   storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+            //   //   messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+            //   //   appId: import.meta.env.VITE_FIREBASE_APP_ID,
+            //   //   measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+            //   // },
+            //   config: {
+            //     apiKey: 'AIzaSyD3Ck4f7Q3PIJ_WRqIdsRagwbISjrrBAGM',
+            //     authDomain: 'bqlda-hoc-mon.firebaseapp.com',
+            //     projectId: 'bqlda-hoc-mon',
+            //     storageBucket: 'bqlda-hoc-mon.firebasestorage.app',
+            //     messagingSenderId: '568867220435',
+            //     appId: '1:568867220435:web:973b81f944108377c6dd26',
+            //     measurementId: 'G-334Q9F6EMG',
+            //   },
+            // });
+          }
+        })
+        .catch(error => {
+          console.error('Đăng ký thất bại:', error);
+        });
+    } else {
+      console.log('Service Worker đã được đăng ký trước đó');
+    }
+  });
+}
 
-// navigator.serviceWorker.addEventListener('message', event => {
-//   console.log('Client nhận được phản hồi từ SW:', event.data);
-// });
+navigator.serviceWorker.addEventListener('message', event => {
+  console.log('Client nhận được phản hồi từ SW:', event.data);
+});
 
 const updateTabCount = () => {
   const tabCount = parseInt(localStorage.getItem(TAB_KEY) || '0', 10);
